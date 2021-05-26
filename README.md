e7625a4925a1eb005127018a7c1aaa8eba96349e
xs1hao <42058817+xs1hao@users.noreply.github.com>
2021-05-26 21:45:52 +0800

    2021年5月26日21:45:49

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NgWyy
-
+加载这个angular项目，需要先启动接口项目；
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.0.
 
 ## 启动接口（[网易云音乐接口](NeteaseCloudMusicApi)）：
--- src/app/utils/array.ts
+++ src/app/utils/array.ts
@@ -9,7 +9,7 @@ export function shuffle<T>(arr: T[]): T[] {
   const result = arr.slice();
   for (let i = 0; i < result.length; i++) {
 
-    // 0和i 之间的一个随机数
+    // 0和 i 之间的一个随机数
     const j = getRandomInt([0, i]);
 
     [result[i], result[j]] = [result[j], result[i]];
